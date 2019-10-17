0d1889328704ec9d52657cc67559ed383245a4ef
Alexey Yurchenko <alexeyumka@gmail.com>
2019-10-17 13:18:12 +0300

    Simple ONION type registration system, beta admin control

--- PcComponentsShop.UI/Controllers/HomeController.cs
+++ PcComponentsShop.UI/Controllers/HomeController.cs
@@ -3,34 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using PcComponentsShop.Domain.Core.Basic_Models.RegistrationSystemModels;
+using PcComponentsShop.Infrastructure.Data.RegistrationSystemManagment;
 using PcComponentsShop.Infrastructure.Data.Units;
 
 namespace PcComponentsShop.UI.Controllers
 {
-    [RequireHttps]
     public class HomeController : Controller
     {
-        PcComponentsUnit PcComponentsUnit;
-        public HomeController()
-        {
-            PcComponentsUnit = new PcComponentsUnit();
-        }
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
---
+++ PcComponentsShop.UI/Helpers/IdentityHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using PcComponentsShop.Infrastructure.Data.RegistrationSystemManagment;
+
+namespace PcComponentsShop.UI.Helpers
+{
+    public static class IdentityHelpers
+    {
+        public static MvcHtmlString GetUserName(this HtmlHelper html, string id)
+        {
+            AppUserManager mgr = HttpContext.Current
+                .GetOwinContext().GetUserManager<AppUserManager>();
+
+            return new MvcHtmlString(mgr.FindByIdAsync(id).Result.UserName);
+        }
+    }
+}
